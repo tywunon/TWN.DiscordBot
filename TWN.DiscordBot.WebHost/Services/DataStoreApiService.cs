035584df116c5ba5a493c3a257c43e1776f58dd9
tywunon <tywunon@gmx.net>
2024-10-09 02:22:37 +0200

    fix: let AddAnnouncementAsync check if twitchUser exists

--- TWN.DiscordBot.WebHost/Services/DataStoreApiService.cs
+++ TWN.DiscordBot.WebHost/Services/DataStoreApiService.cs
@@ -4,11 +4,10 @@ using TWN.DiscordBot.Interfaces;
 using TWN.DiscordBot.Interfaces.Types;
 
 namespace TWN.DiscordBot.WebHost.Services;
-internal class DataStoreApiService(IDataStoreAsync dataStore) : IDataStoreApiServiceAsync
+internal class DataStoreApiService(IDataStoreAsync dataStore, ITwitchClientAsync twitchClient) : IDataStoreApiServiceAsync
 {
   public async Task<IResult> GetAnnouncementsAsync(CancellationToken cancellationToken)
   {
-
     var result = await dataStore.GetAnnouncementsAsync(cancellationToken);
     return Results.Ok(new ResultMessage<Payloads.AnnouncementsPayload>()
     {
@@ -20,13 +19,28 @@ internal class DataStoreApiService(IDataStoreAsync dataStore) : IDataStoreApiSer
 
   public async Task<IResult> AddAnnouncementAsync(string twitchUser, ulong guildID, ulong channelID, CancellationToken cancellationToken)
   {
-    var result = await dataStore.AddAnnouncementAsync(twitchUser, guildID, channelID, cancellationToken);
-    return Results.Ok(new ResultMessage<Payloads.AnnouncementPayload>()
-    {
-      Success = true,
-      Message = string.Empty,
-      Payload = new (result),
-    });
+    var userResult = await twitchClient.GetUsersAsync([twitchUser], cancellationToken);
+    return await userResult.Match(
+      async s => 
+      {
+        var result = await dataStore.AddAnnouncementAsync(twitchUser, guildID, channelID, cancellationToken);
+        return Results.Ok(new ResultMessage<Payloads.AnnouncementPayload>()
+        {
+          Success = true,
+          Message = string.Empty,
+          Payload = new(result),
+        });
+      },
+      err => 
+      {
+        return Task.FromResult(Results.Ok(new ResultMessage<Payloads.AnnouncementPayload>()
+        {
+          Success = false,
+          Message = err.Value.Message ?? string.Empty,
+          Payload = new Payloads.AnnouncementPayload(new Announcement(twitchUser, 0L, 0L)),
+        }));
+      }
+      );
   }
 
   public async Task<IResult> DeleteAnnouncementAsync(string twitchUser, ulong guildID, ulong? channelID, CancellationToken cancellationToken)
